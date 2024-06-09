e6e2fcd7e189926339294fcad1f87a40bf570e5e
Matthew Ameh <matthewameh007@gmail.com>
2024-06-09 10:08:21 +0100

    contact page fixed

--- src/app/(components)/ProjectHome.jsx
+++ src/app/(components)/ProjectHome.jsx
@@ -7,7 +7,7 @@ import Project from "./ProjectPage";
 const ProjectHome = () => {
   return (
     <div className="w-full h-auto  overflow-hidden">
-      <div className="gradient-effect">
+      <div className="gradient-effect w-full h-screen">
         <div className="circle circle3"></div>
         <div className="circle circle4"></div>
       </div>
