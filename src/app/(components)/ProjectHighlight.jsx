0166d56908f31f21d947be55a48e1313b7e8f631
Matthew Ameh <matthewameh007@gmail.com>
2024-06-10 10:51:14 +0100

    hero-testimonial

--- src/app/(components)/ProjectHighlight.jsx
+++ src/app/(components)/ProjectHighlight.jsx
@@ -1,5 +1,143 @@
+'use client'
 import Link from "next/link";
-import React from "react";
+import React, {useState} from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from "swiper/modules";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus, faX } from "@fortawesome/free-solid-svg-icons";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
+
+const testimonials = [
+  {
+    id: 1,
+    tech: "testimonials",
+    content:
+      "Stefano is an experienced professional, curious and always looking for new ideas and inspirations. He has an excellent aptitude for teamwork and discussion, and it was a pleasure to work with him both for his approach and his skills.",
+    author: "Alessandro Tunno",
+    position: "Independent Creative Director",
+    image: "/images/top.jpg",
+  },
+  {
+    id: 2,
+    tech: "testimonials",
+    content:
+      "Stefano takes the development process seriously. His knowledge is inspiring and always appreciated. I highly recommend Stefano.",
+    author: "Riccardo Marconato",
+    position: "Independent Creative Director",
+    image: "/images/top.jpg",
+  },
+  {
+    id: 3,
+    tech: "testimonials",
+    content:
+      "Absolute pleasure working with Stefano. He has a knack for accurate implementations and state-of-the-art technical solutions.",
+    author: "Giuseppe Capizzi",
+    position: "Digital Designer & Founder @ Vool",
+    image: "/images/top.jpg",
+  },
+  {
+    id: 4,
+    tech: "testimonials",
+    content:
+      "Absolute pleasure working with Stefano. He has a knack for accurate implementations and state-of-the-art technical solutions.",
+    author: "Giuseppe Capizzi",
+    position: "Digital Designer & Founder @ Vool",
+    image: "/images/top.jpg",
+  },
+  
+];
+
+const TestimonialCardhighlight = ({ tech, content, author, position, image }) => (
+  <div className=" mt-8 testimonial-card w-72 md:w-full ">
+    <div className="about-me-title p-3">
+      <p className="text-sm font-semibold">{tech}</p>
+      <div className="flex space-x-4 text-gray-500 text-xs">
+        <FontAwesomeIcon
+          icon={faMinus}
+          className="hover:text-white cursor-pointer"
+          style={{ transition: "ease-in 0.5s" }}
+        />
+        <div
+          className="w-2.5 h-2.5 border border-gray-500 rounded-sm hover:border-white cursor-pointer"
+          style={{ transition: "ease-in 0.5s" }}
+        ></div>
+        <FontAwesomeIcon
+          icon={faX}
+          className="hover:text-white cursor-pointer"
+          style={{ transition: "ease-in 0.5s" }}
+        />
+      </div>
+    </div>
+    <div className="testimonial-content p-5">
+      <p className="testimonial-text">{content}</p>
+      <p className="testimonial-author">
+        {author} &#123;&#125; {position}
+      </p>{" "}
+      <img src={image} alt={author} className="author-image" />
+    </div>
+  </div>
+);
+
+const TestimonialHighlight = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const slidesPerView = 3;
+
+  const isMiddleSlide = (index) => {
+    const midIndex = (slidesPerView - 1) / 2;
+    return index === activeIndex + midIndex;
+  };
+
+  return (
+    <>
+      <h1 className="poppins text-4xl md:text-6xl text-center">
+        Nice things <span className="all-text">people say</span>
+        <br />
+        about my work
+      </h1>
+
+      <div className="relative w-full h-screen flex justify-center items-center p-4">
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+          spaceBetween={80}
+          slidesPerView={2}
+          loop={true}
+          autoplay={{ delay: 5000, disableOnInteraction: true }}
+          onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
+          className=" cursor-grab testimonial-swiper"
+        >
+          {testimonials.map(
+            ({ id, tech, content, author, position, image }, index) => (
+              <SwiperSlide
+                key={id}
+                className={isMiddleSlide(index) ? "active-slide" : ""}
+              >
+                <TestimonialCardhighlight
+                  tech={tech}
+                  content={content}
+                  author={author}
+                  position={position}
+                  image={image}
+                />
+              </SwiperSlide>
+            )
+          )}
+        </Swiper>
+      </div>
+    </>
+  );
+};
 
 const projects = [
   {
@@ -24,7 +162,7 @@ const AllProjects = () => {
   return (
     <>
       <div
-        className="w-full flex flex-col items-center mt-10
+        className="w-full flex flex-col items-center mt-10 mb-28
       "
       >
         <div className="w-full flex flex-col items-center space-y-4  mt-30 poppins">
@@ -79,7 +217,7 @@ const ProjectHighlight = () => {
         Project <span className="all-text">higlights</span>
       </h1>
       <AllProjects />
-     
+     <TestimonialHighlight/>
     </div></>
   );
 };
--- src/app/(components)/Testimonial.jsx
+++ src/app/(components)/Testimonial.jsx
@@ -1,58 +1,67 @@
 import React, { useState } from "react";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from "swiper/modules";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMinus, faX } from "@fortawesome/free-solid-svg-icons";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import 'swiper/css/autoplay'; 
-
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
 
 const testimonials = [
   {
     id: 1,
     tech: "testimonials",
-    content: "Stefano is an experienced professional, curious and always looking for new ideas and inspirations. He has an excellent aptitude for teamwork and discussion, and it was a pleasure to work with him both for his approach and his skills.",
+    content:
+      "Stefano is an experienced professional, curious and always looking for new ideas and inspirations. He has an excellent aptitude for teamwork and discussion, and it was a pleasure to work with him both for his approach and his skills.",
     author: "Alessandro Tunno",
     position: "Independent Creative Director",
-    image: "/images/top.jpg"
+    image: "/images/top.jpg",
   },
   {
     id: 2,
     tech: "testimonials",
-    content: "Stefano takes the development process seriously. His knowledge is inspiring and always appreciated. I highly recommend Stefano.",
+    content:
+      "Stefano takes the development process seriously. His knowledge is inspiring and always appreciated. I highly recommend Stefano.",
     author: "Riccardo Marconato",
     position: "Independent Creative Director",
-    image: "/images/top.jpg"
+    image: "/images/top.jpg",
   },
   {
     id: 3,
     tech: "testimonials",
-    content: "Absolute pleasure working with Stefano. He has a knack for accurate implementations and state-of-the-art technical solutions.",
+    content:
+      "Absolute pleasure working with Stefano. He has a knack for accurate implementations and state-of-the-art technical solutions.",
     author: "Giuseppe Capizzi",
     position: "Digital Designer & Founder @ Vool",
-    image: "/images/top.jpg"
+    image: "/images/top.jpg",
   },
   {
     id: 4,
     tech: "testimonials",
-    content: "Absolute pleasure working with Stefano. He has a knack for accurate implementations and state-of-the-art technical solutions.",
+    content:
+      "Absolute pleasure working with Stefano. He has a knack for accurate implementations and state-of-the-art technical solutions.",
     author: "Giuseppe Capizzi",
     position: "Digital Designer & Founder @ Vool",
-    image: "/images/top.jpg"
+    image: "/images/top.jpg",
   },
   {
     id: 5,
     tech: "testimonials",
-    content: "Absolute pleasure working with Stefano. He has a knack for accurate implementations and state-of-the-art technical solutions.",
+    content:
+      "Absolute pleasure working with Stefano. He has a knack for accurate implementations and state-of-the-art technical solutions.",
     author: "Giuseppe Capizzi",
     position: "Digital Designer & Founder @ Vool",
-    image: "/images/top.jpg"
+    image: "/images/top.jpg",
   },
-
 ];
 
 const TestimonialCard = ({ tech, content, author, position, image }) => (
@@ -77,9 +86,11 @@ const TestimonialCard = ({ tech, content, author, position, image }) => (
       </div>
     </div>
     <div className="testimonial-content p-5">
-  
       <p className="testimonial-text">{content}</p>
-      <p className="testimonial-author">{author} &#123;&#125; {position}</p>    <img src={image} alt={author} className="author-image" />
+      <p className="testimonial-author">
+        {author} &#123;&#125; {position}
+      </p>{" "}
+      <img src={image} alt={author} className="author-image" />
     </div>
   </div>
 );
@@ -98,30 +109,36 @@ const Testimonial = () => {
     <>
       <h1 className="poppins text-4xl md:text-6xl text-center">
         Nice things <span className="text-blue-600">people say</span>
-        <br />about my work
+        <br />
+        about my work
       </h1>
 
       <div className="relative w-full h-screen flex justify-center items-center p-4">
         <Swiper
-          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]} 
+          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
           spaceBetween={80}
           slidesPerView={2}
-          loop={true} 
-          autoplay={{ delay: 5000, disableOnInteraction: true }} 
+          loop={true}
+          autoplay={{ delay: 5000, disableOnInteraction: true }}
           onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
           className=" cursor-grab testimonial-swiper"
         >
-          {testimonials.map(({ id, tech, content, author, position, image }, index) => (
-            <SwiperSlide key={id} className={isMiddleSlide(index) ? 'active-slide' : ''}>
-              <TestimonialCard
-                tech={tech}
-                content={content}
-                author={author}
-                position={position}
-                image={image}
-              />
-            </SwiperSlide>
-          ))}
+          {testimonials.map(
+            ({ id, tech, content, author, position, image }, index) => (
+              <SwiperSlide
+                key={id}
+                className={isMiddleSlide(index) ? "active-slide" : ""}
+              >
+                <TestimonialCard
+                  tech={tech}
+                  content={content}
+                  author={author}
+                  position={position}
+                  image={image}
+                />
+              </SwiperSlide>
+            )
+          )}
         </Swiper>
       </div>
     </>
