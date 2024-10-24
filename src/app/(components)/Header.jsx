a9d2148a0467348ed8aeca1627e54288c2894de6
Matthew Ameh <matthewameh007@gmail.com>
2024-10-24 16:03:47 +0100

    added fixed stule

--- src/app/(components)/Header.jsx
+++ src/app/(components)/Header.jsx
@@ -1,6 +1,6 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import AnimatedLetters from "./AnimatedLetters";
-import './App.scss'
+import './App.scss';
 
 const Header = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
@@ -8,9 +8,8 @@ const Header = () => {
 
   return (
     <>
-      <main className="w-full header" >
+      <main className="w-full header">
         <div className="relative z-10 flex flex-col text-center items-center mt-32 ">
-          {" "}
           <div className="words-container">
             <div className="word">
               <p>&lt;h1&gt;</p>
@@ -21,23 +20,24 @@ const Header = () => {
           <span className="text-xs poppins text-gray-500 uppercase tracking-wider">
             Home
           </span>
-          <h1 className=" w-[94%] text-5xl md:text-7xl lg:text-[110px] font-normal  mt-4 lg:w-10/12 poppins g">
-            Hi<span className=" font-serif">&#44;</span> I
+          <h1 className="w-[93%] text-[46px] md:text-7xl lg:text-[110px] font-normal mt-4 lg:w-10/12 poppins g">
+            Hi<span className="font-serif">&#44;</span> I
             <span className="font-serif">&#39;</span>
             <AnimatedLetters
               letterClass={letterClass}
               strArray={nameArray}
               idx={15}
             />
-            <br /> <span className="all-text"> creative </span>developer
+            {/* Use Tailwind to hide/show the <br /> tag on different screen sizes */}
+            <br className="hidden md:block" /> {/* Only visible on medium screens and larger */}
+            <span className="all-text"> creative </span>developer
           </h1>
-          <p className=" w-11/12 md:w-10/12 lg:w-6/12 2xl:w-5/12 md:text-base text-[13px] max-w-2xl text-gray-400 mt-8 our-text">
+          <p className="w-11/12 md:w-10/12 lg:w-6/12 2xl:w-5/12 md:text-base text-[13px] max-w-2xl text-gray-400 mt-8 our-text">
             I bring value to web development projects by merging technical
             expertise with creativity and aesthetics.
           </p>
         </div>
       </main>
-    
     </>
   );
 };
