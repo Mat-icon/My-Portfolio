9c087dd4a452617aa037c8f8b4069ba37e3cb416
Matthew Ameh <matthewameh007@gmail.com>
2024-10-31 15:25:10 +0100

    added navabar style

--- src/app/(components)/Loader.jsx
+++ src/app/(components)/Loader.jsx
@@ -9,17 +9,29 @@ const Loader = () => {
         <div className="circle circle1"></div>
         <div className="circle circle2"></div>
       </div>
-      <header className="w-7/12  h-12 flex justify-center items-center  relative z-10">
+
+      <div className="w-11/12 md:w-5/12  border border-[#81808079] bg-[#00000027] rounded-[4px] h-12 md:h-14 relative z-10 flex items-center ">
+       <div className="w-[15%] md:w-[10%] font-bold flex items-center justify-center">M</div>
+       <div className="w-[1px] bg-[#81808079] h-full"></div>
+       <div className="w-[70%] md:w-[80%] flex items-center justify-center">
+          <span className="text-base md:text-lg font-medium text-center fonts">
+            matthew
+            <span className="text-base md:text-lg all-text">&lt;ameh&gt;</span>
+          </span>
+        </div>
+        <div className="w-[1px] bg-[#81808079] h-full"></div>
+        <header className="w-[15%] md:w-[10%]  h-12 flex justify-center items-center  relative z-10">
         <Rings
           visible={true}
-          height="100"
-          width="100"
+          height="60"
+          width="60"
           color="#4fa94d"
           ariaLabel="rings-loading"
           wrapperStyle={{}}
           wrapperClass=""
         />
-      </header>
+        </header>
+      </div>
     </div>
   );
 };
