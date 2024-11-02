fcf4f2435bba5df3bfcc99306c498dd1238412a8
Matthew Ameh <matthewameh007@gmail.com>
2024-11-02 13:58:17 +0100

    added changes to design

--- src/app/(components)/ContactPage.jsx
+++ src/app/(components)/ContactPage.jsx
@@ -4,7 +4,7 @@ import Contacts from './Contacts'
 const ContactPage = () => {
   return (
     <div className="w-full h-auto">
-    <div className="gradient-effect w-full h-screen">
+    <div className="gradient-effect w-full h-screen overflow-hidden">
       <div className="circle circle7"></div>
       <div className="circle circle8"></div>
     </div>
--- src/app/(components)/ProjectHome.jsx
+++ src/app/(components)/ProjectHome.jsx
@@ -6,8 +6,8 @@ import Project from "./ProjectPage";
 
 const ProjectHome = () => {
   return (
-    <div className="w-full h-auto  overflow-hidden">
-      <div className="gradient-effect w-full h-screen">
+    <div className="w-full h-auto  ">
+      <div className="gradient-effect w-full h-screen overflow-hidden">
         <div className="circle circle3"></div>
         <div className="circle circle4"></div>
       </div>
