0a6371242ce1da512125df1bc72ce306086f707e
Matthew Ameh <matthewameh007@gmail.com>
2025-11-09 18:05:39 +0100

    feat/services

--- src/app/projects/page.jsx
+++
@@ -1,9 +0,0 @@
-import ProjectPage from "../(components)/ProjectPage"
-
-const page = ({currentRoute}) => {
-  return (
-    <><ProjectPage currentRoute={currentRoute}  /></>
-  )
-}
-
-export default page
