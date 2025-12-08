b0c1b492569241981b4c6d7693b46a43d4e3bcba
Matthew Ameh <matthewameh007@gmail.com>
2025-12-08 07:09:43 +0100

    added glass

--- next.config.js
+++ next.config.js
@@ -1,27 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    turbo: false, // FORCES WEBPACK (Fixes your Netlify + Turbopack error)
+  },
 
-module.exports = nextConfig
-
-// next.config.js
-module.exports = {
-    webpack: (config, { isServer }) => {
-      // Add support for loading .mov files
-      config.module.rules.push({
-        test: /\.(mp4|mov)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              publicPath: '/_next',
-              name: 'static/media/[name].[hash].[ext]',
-            },
+  webpack: (config) => {
+    // Add support for loading .mov & .mp4 files
+    config.module.rules.push({
+      test: /\.(mp4|mov)$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            publicPath: "/_next",
+            name: "static/media/[name].[hash].[ext]",
           },
-        ],
-      });
-  
-      // Important: return the modified config
-      return config;
-    },
-  };
-  
+        },
+      ],
+    });
+
+    return config;
+  }
+};
+
+module.exports = nextConfig;
